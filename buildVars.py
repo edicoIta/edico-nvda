affc79438cc322eca6d681c091e7af895633244c
albzan <lapostadialberto@gmail.com>
2022-09-15 08:50:13 +0200

    updated version

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("""This addon provides access to the EDICO scientific editor"""),
 	# version
-	"addon_version": "1.0",
+	"addon_version": "1.1",
 	# Author(s)
 	"addon_author": "Alberto Zanella - I.Ri.Fo.R. - https://www.irifor.eu/",
 	# URL for the add-on documentation support
